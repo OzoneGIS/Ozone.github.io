290b5667013c7a37d910a51de84978c6ced89d5b
Adrian <adarian@ucmerced.edu>
2018-04-04 16:48:30 -0700

    index js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import {
+    HashRouter,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+import App from 'containers/App/App.jsx';
+
+import './assets/css/bootstrap.min.css';
+import './assets/css/animate.min.css';
+import './assets/sass/light-bootstrap-dashboard.css';
+import './assets/css/demo.css';
+import './assets/css/pe-icon-7-stroke.css';
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
+
+
+ReactDOM.render((
+    <HashRouter>
+        <Switch>
+            <Route path="/" name="Home" component={App}/>
+        </Switch>
+    </HashRouter>
+),document.getElementById('root'));
