d22f19e5651f7ea767794c57734f5571b7a48b76
adarian <adarian@ucmerced.edu>
2018-04-06 01:57:46 -0700

    something was wrong

---
+++ client/src/routes/app.jsx
@@ -0,0 +1,14 @@
+import Dashboard from 'views/Dashboard/Dashboard';
+//import UserProfile from 'views/UserProfile/UserProfile';
+//import Icons from 'views/Icons/Icons';
+import Maps from 'views/Maps/Maps';
+
+const appRoutes = [
+    { path: "/dashboard", name: "Dashboard", icon: "pe-7s-graph", component: Dashboard },
+    { path: "/maps", name: "Maps", icon: "pe-7s-map-marker", component: Maps },
+    //{ path: "/user", name: "User Profile", icon: "pe-7s-user", component: UserProfile },
+    //{ path: "/icons", name: "Icons", icon: "pe-7s-science", component: Icons },
+    { redirect: true, path:"/", to:"/maps", name: "Maps" }
+];
+
+export default appRoutes;
