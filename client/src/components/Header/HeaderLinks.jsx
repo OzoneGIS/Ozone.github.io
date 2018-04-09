1470ee8e7f91486a476b88baaa1b38fadc014854
Rosalina Aranda <raranda@Rosalinas-iMac.local>
2018-04-08 19:59:13 -0700

    adding pitch and bering options

--- client/src/components/Header/HeaderLinks.jsx
+++ client/src/components/Header/HeaderLinks.jsx
@@ -4,14 +4,7 @@ import { NavItem, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class HeaderLinks extends Component{
     render(){
-        const notification = (
-            <div>
-                <i className="fa fa-globe"></i>
-                <b className="caret"></b>
-                <span className="notification">5</span>
-                <p className="hidden-lg hidden-md">Notification</p>
-            </div>
-        );
+
         return (
             <div>
                 <Nav>
@@ -19,20 +12,8 @@ class HeaderLinks extends Component{
                         <i className="fa fa-dashboard"></i>
                         <p className="hidden-lg hidden-md">Dashboard</p>
                     </NavItem>
-                    <NavDropdown eventKey={2} title={notification} noCaret id="basic-nav-dropdown">
-                        <MenuItem eventKey={2.1}>Notification 1</MenuItem>
-                        <MenuItem eventKey={2.2}>Notification 2</MenuItem>
-                        <MenuItem eventKey={2.3}>Notification 3</MenuItem>
-                        <MenuItem eventKey={2.4}>Notification 4</MenuItem>
-                        <MenuItem eventKey={2.5}>Another notifications</MenuItem>
-                    </NavDropdown>
-                    <NavItem eventKey={3} href="#">
-                        <i className="fa fa-search"></i>
-                        <p className="hidden-lg hidden-md">Search</p>
-                    </NavItem>
                 </Nav>
                 <Nav pullRight>
-                    <NavItem eventKey={1} href="#">Account</NavItem>
                     <NavDropdown eventKey={2} title="Dropdown" id="basic-nav-dropdown-right">
                         <MenuItem eventKey={2.1}>Action</MenuItem>
                         <MenuItem eventKey={2.2}>Another action</MenuItem>
@@ -42,7 +23,10 @@ class HeaderLinks extends Component{
                         <MenuItem divider />
                         <MenuItem eventKey={2.5}>Separated link</MenuItem>
                     </NavDropdown>
-                    <NavItem eventKey={3} href="#">Log out</NavItem>
+                    <NavItem eventKey={3} href="#">
+                        <i className="fa fa-search"></i>
+                        <p className="hidden-lg hidden-md">Search</p>
+                    </NavItem>
                 </Nav>
             </div>
         );
